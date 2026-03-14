7905dbe250580ef1c47c23357edc9d0a5a561d31
chanwoooh <ochanwoo0615@gmail.com>
2026-03-13 22:50:20 -0400

    Day7

---
+++ Python/Easy/0066_plus_one.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        digits[len(digits)-1] += 1
+        for i in range(len(digits)-1, 0, -1):
+            if digits[i] == 10:
+                digits[i] = 0
+                digits[i-1] += 1
+        if digits[0] == 10:
+            digits[0] = 0
+            digits.insert(0, 1)
+        return digits
+
+if __name__ == "__main__":
+    assert Solution().plusOne([1, 2, 3]) == [1, 2, 4]
+    assert Solution().plusOne([4, 3, 2, 1]) == [4, 3, 2, 2]
+    assert Solution().plusOne([9]) == [1, 0]
+
+    print("All test cases passed!")
